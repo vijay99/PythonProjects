7fe07f65ebcef97d0215451a662cfd87eb48b42d
Kumar Vijay <vijay81238@gmail.com>
2022-12-25 20:20:04 +0530

    Initial commit : adding guess number project

---
+++ guess_number/guess_number.py
@@ -0,0 +1,35 @@
+import random
+
+print('Welcome to number guess game!')
+top_of_range = input('Type a number:')
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print('Please input number greater then 0 next time.')
+        quit()
+else:
+    print('Please type a number next time')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+# print(random_number)
+guesses = 0
+while True:
+    guesses += 1
+    user_guess = input("Make a guess:")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time')
+        continue
+
+    if user_guess == random_number:
+        print('You got it right!')
+        break
+    elif user_guess > random_number:
+        print('You are above the number!')
+    else:
+        print('You are below the number')
+
+print('You got it in', guesses, "guesses")
